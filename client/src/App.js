5ff0b9920d67185965143400e8fbf90d1c298906
jusrockinout <gary.newton@ymail.com>
2017-08-08 13:33:04 -0400

    Working on client directory; deleted public directory and moved content into src

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,9 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
-import $ from 'jquery';
+// import $ from 'jquery';
+import Header from './Components/Header';
 import Navbar from './Components/Navbar';
-import Banner from './Components/Banner';
+import About from './Components/About';
 import Search from './Components/Search';
 import Map from './Components/Map';
 import Contact from './Components/Contact';
@@ -12,15 +12,17 @@ import Footer from './Components/Footer';
 
 class App extends Component {
   render() {
-    return (
+    console.log("render: True");
+     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <Header />
+        <Navbar />
+        <About />
+        <Search />
+        <Map />
+        <Contact />
+        <Register />
+        <Footer />
       </div>
     );
   }
